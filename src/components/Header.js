82d34568c9ed473ee71c499001a164b85cbdbaee
Руслан Бакиров <ruslanbakirov@MacBook-Air-Ruslan.local>
2022-02-15 17:17:37 +0300

    fix: Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,10 @@ function Header() {
     return (
         <header className="header">
             <div className="header__logo"></div>
+            <div className="header__login-container">
+                <p className="header__user-mail">email@mail.com</p>
+                <button className="header__button-exit">Выйти</button>
+            </div>
         </header>
     )
 }
